40a2bb1ad02e8e4775644166d502a23d65555c99
Matt <39172048+magiccreb@users.noreply.github.com>
2022-11-23 20:17:24 -0500

    added assignment.md and main.js with a few scribbles.

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+/* 11/23/2022
+- o hey great to be here
+*/
+
+// OpenWeather 5-day API key
+var myKey = '69408b91f433def6af6c538d44d5e090'
+
+// apiUrl = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
