9fb92f8d7fef47fb1909d19475f9f9792a744d7c
Matt <39172048+magiccreb@users.noreply.github.com>
2022-11-24 19:27:26 -0500

    set up html skeleton based on wireframe and pseudocode. established some early CSS and began scoping JS functionality in commentary.

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,8 @@
 // OpenWeather 5-day API key
 var myKey = '69408b91f433def6af6c538d44d5e090'
 
-// apiUrl = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// geocodingUrl = http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API key}
+// 5DayUrl = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// need a function to create an object containing the searched city name and its lat and lon to plug into the 5DayUrl
+// Array.unshift will add an object to the array's first position.
