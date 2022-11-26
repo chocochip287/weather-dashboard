f952947cb2d75bb1c93ce2b01963ef3c7dedbb75
Matt <39172048+magiccreb@users.noreply.github.com>
2022-11-26 16:40:07 -0500

    set up search button event listener to establish a dynamic variable to plug into the geocodingUrl.

--- assets/main.js
+++ assets/main.js
@@ -5,8 +5,28 @@
 // OpenWeather 5-day API key
 var myKey = '69408b91f433def6af6c538d44d5e090'
 
-// geocodingUrl = http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API key}
+// variable to ID the search textarea
+
+var searchedCity = document.getElementById('searchedCity');
+
+// variable to ID the search button
+
+var searchButton = document.getElementById('searchButton');
+
+// event listener to set the searched value as the city variable
+
+searchButton.addEventListener('click', setCity);
+
 // 5DayUrl = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 // need a function to create an object containing the searched city name and its lat and lon to plug into the 5DayUrl
 // Array.unshift will add an object to the array's first position.
+
+function setCity() {
+    console.log('the city should be set');
+    city = searchedCity.value;
+}
+
+var city = "";
+var geocodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + myKey;
+
